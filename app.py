23cf0f4edec3c5798384886d267c416b6f38afa2
Wesley Ong <72511481+Wesleyongs@users.noreply.github.com>
2021-11-22 18:28:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ HTML_BANNER = """
 stc.html(HTML_BANNER)
 
 # Side bar
-for i in ['summary-data','add-new-order','remove-order']:
-    st.sidebar.markdown(f"<a href='#{i}'>{i}</a>", unsafe_allow_html=True)
+# for i in ['summary-data','add-new-order','remove-order']:
+#     st.sidebar.markdown(f"<a href='#{i}'>{i}</a>", unsafe_allow_html=True)
 
 # Selector pannel
 sc = st.container()
@@ -59,21 +59,24 @@ for col in itertools.cycle(columns):
 sc.write(df)
 
 # Add new data 
-with st.form(key='my_form'):
+with st.sidebar.form(key='my_form'):
 
     st.header("Add New Order")
     form_dict = {}
     form_dict['date'] = st.date_input('Date', value=dt.date.today(), min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None)
     form_dict['address'] = st.text_input('Address', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None)
-    cols = st.columns(3)
-    for cycle in range(0, math.ceil(len(products)/3)):
-        for i, col in enumerate(cols):
-            i = (i)+(cycle*3)
-            try:
-                product = products[i]
-                form_dict[product] = col.number_input(product, min_value=0, key=i)
-            except:
-                pass
+    # cols = st.columns(3)
+    # for cycle in range(0, math.ceil(len(products)/3)):
+    #     for i, col in enumerate(cols):
+    #         i = (i)+(cycle*3)
+    #         try:
+    #             product = products[i]
+    #             form_dict[product] = col.number_input(product, min_value=0, key=i)
+    #         except:
+    #             pass
+    for i, col in enumerate(range(len(products))):
+        product = products[i]
+        form_dict[product] = st.number_input(product, min_value=0, key=i)
     form_dict['Author'] = st.selectbox("Author", ['XX','WES','Ku','Evan'], index=0, key=None, help=None, on_change=None, args=None, kwargs=None)
     submit_button = st.form_submit_button(label='Add Order')
     
@@ -84,7 +87,7 @@ with st.form(key='my_form'):
         # st.markdown("<a href='#summary-data'>View Changes</a>", unsafe_allow_html=True)
     
 # Delete row
-with st.form(key='delete_form'):
+with st.sidebar.form(key='delete_form'):
 
     st.header("Remove Order")
     del_index = st.multiselect('Index of row you want to delete', df.index)
@@ -94,6 +97,4 @@ if del_submit_button:
     df = df.drop(del_index)
     df.to_csv('database.csv', index=False)
 
-# Save data
-if st.button('Save Changes', key=None, help=None, on_click=None, args=None, kwargs=None): df.to_csv('database.csv', index=False)
 
